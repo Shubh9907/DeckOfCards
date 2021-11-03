9b2c336e92b74e9301cddabf7cd40dbfa01322f0
Shubh9907 <shubhamv60047@gmail.com>
2021-11-03 08:30:29 +0530

    Added Welcome Message

--- src/com/bridgelabz/DeckOfCards.java
+++ src/com/bridgelabz/DeckOfCards.java
@@ -1,6 +1,6 @@
 package com.bridgelabz;
 
-public class Main {
+public class DeckOfCards {
 
     public static void main(String[] args) {
         System.out.println("Welcome to the Deck of Cards");
